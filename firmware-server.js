26fd032a1410952e645790e761f904f742b7a30d
Jacob Lewallen <jlewallen@gmail.com>
2019-10-24 10:52:19 -0700

    Serve from symlinks.

--- firmware-server.js
+++ firmware-server.js
@@ -1,27 +1,49 @@
 const path = require('path');
 const express = require("express");
+const fs = require('fs');
 const app = express();
 
+const port = 6060;
 const home = process.env.HOME;
-const localBuild = path.join(home, "fieldkit/firmware/build/samd51/");
+const local = path.join(home, "fieldkit/dev-env/archive/local");
+const archived = "archive/fk-firmware-266";
+const serving = local;
 
 function applicationFirmware(req, res) {
-    console.log(req.params);
-    res.sendFile("fk/fk-bundled-fkb.bin", { root: localBuild });
+    const name = "fk-bundled-fkb.bin";
+    fs.realpath(path.join(serving, name), (err, real) => {
+        if (err) {
+            console.log('error', err);
+            return;
+        }
+        console.log(req.connection.remoteAddress, real, req.params);
+        res.sendFile(real);
+    });
 }
 
 function bootloaderFirmware(req, res) {
-    console.log(req.params);
-    res.sendFile("bootloader/fkbl-fkb.bin", { root: localBuild });
+    const name = "fkbl-fkb.bin";
+    fs.realpath(path.join(serving, name), (err, real) => {
+        if (err) {
+            console.log('error', err);
+            return;
+        }
+        console.log(req.connection.remoteAddress, real, req.params);
+        res.sendFile(real);
+    });
 }
 
 app.get("/:deviceId/fk-bundled-fkb.bin", applicationFirmware);
 app.get("/:deviceId/fkbl-fkb.bin", bootloaderFirmware);
 
+app.get("/fk-bundled-fkb.bin", applicationFirmware);
+app.get("/fkbl-fkb.bin", bootloaderFirmware);
+
 app.get("*", (req, res, n) => {
     res.status(404).send("404 not found");
 });
 
-app.listen(6060, () => {
-    console.log("Listening on :6060");
+app.listen(port, () => {
+    console.log("Listening on :" + port);
+    console.log("Serving", serving)
 });
