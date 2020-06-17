9653e76f50d021cea215627f6f3a10bc991c1572
Jacob Lewallen <jlewallen@gmail.com>
2020-06-17 13:06:27 -0700

    Open up the github page at the start of the script.

--- rename-main.sh
+++ rename-main.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
+if command -v xdg-open; then
+	xdg-open `git remote -v | grep fetch | awk '{print $2}' | sed 's/git@/http:\/\//' | sed 's/com:/com\//'`| head -n1
+else
+	open `git remote -v | grep fetch | awk '{print $2}' | sed 's/git@/http:\/\//' | sed 's/com:/com\//'`| head -n1
+fi
+
 git branch -m master main && git push origin --set-upstream main
 
 echo
@@ -9,11 +15,13 @@ echo browser and please open go to the repository, go to Settings, then
 echo Branches. Here you can change the default branch to "main" and press
 echo update.
 echo
-echo "NOTE: It would be cool if this script just opened that page :)"
-echo
 echo After that is updated, press enter to delete the old branch.
 echo
 
 read -n 1
 
+echo Deleting old branch.
+
 git push origin :master
+
+echo Done!
