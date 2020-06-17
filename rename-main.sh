96bc7db8765d1a8adb381eac315b273cd4f54c15
Jacob Lewallen <jlewallen@gmail.com>
2020-06-17 12:45:53 -0700

    Remember a possible future improvement to this script.

--- rename-main.sh
+++ rename-main.sh
@@ -3,10 +3,13 @@
 git branch -m master main && git push origin --set-upstream main
 
 echo
+echo
 echo For GitHub repositories we need to change the default branch. Open a
 echo browser and please open go to the repository, go to Settings, then
 echo Branches. Here you can change the default branch to "main" and press
 echo update.
+echo
+echo "NOTE: It would be cool if this script just opened that page :)"
 echo
 echo After that is updated, press enter to delete the old branch.
 echo
