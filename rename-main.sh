1684ce0a7e4b7cdbb81db555a8a755ce31297499
Jacob Lewallen <jlewallen@gmail.com>
2020-06-17 12:31:42 -0700

    Script to migrate branch names.

---
+++ rename-main.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+git branch -m master main && git push origin --set-upstream main
+
+echo
+echo For GitHub repositories we need to change the default branch. Open a
+echo browser and please open go to the repository, go to Settings, then
+echo Branches. Here you can change the default branch to "main" and press
+echo update.
+echo
+echo After that is updated, press enter to delete the old branch.
+echo
+
+read -n 1
+
+git push origin :master
